c2b8e0d31cfe529ee28f0ead6f8c2a4f0332b601
Soumik <soumik20020906@gmail.com>
2023-08-15 15:33:36 +0530

    Changed things a little bit!

--- src/main.py
+++ src/main.py
@@ -10,19 +10,21 @@ if not capture.isOpened() :
 ret,frame=capture.read()
 frame=cv.resize(frame,(640,480))
 cars_count=0
-while True:
-	if frame is None:
-		break;
-	# displaying each frame
-	if cv.waitKey(40) & 0xff ==ord('d'):
-		break
-	prev_frame=frame
-	# cv.imshow('Sample',frame)
-	ret,frame=capture.read()
-	frame=cv.resize(frame,(640,480))
-	valid=find_cars(prev_frame,frame)
-	cv.putText(prev_frame,f"Vehicle Detected:{len(valid)}",(55,15),cv.FONT_HERSHEY_COMPLEX,0.6,(0,180,0),2)
-	cv.imshow('Sample2',prev_frame)
-
+playing=True
+while playing:
+		# displaying each frame
+		if cv.waitKey(40) & 0xff ==ord('d'):
+			playing=False
+			break
+		prev_frame=frame
+		# cv.imshow('Sample',frame)
+		ret,frame=capture.read()
+		if ret:
+			frame=cv.resize(frame,(640,480))
+			valid=find_cars(prev_frame,frame)
+			cv.putText(prev_frame,f"Vehicle Detected:{len(valid)}",(55,15),cv.FONT_HERSHEY_COMPLEX,0.6,(0,180,0),2)
+			cv.imshow('Sample2',prev_frame)
+		else :
+			playing=False
 capture.release()
-cv.waitKey(0)
+# cv.waitKey(0)
