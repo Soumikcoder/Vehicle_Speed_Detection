e85bf8edaf6e52be7e5fc2d38ed9bbe94b41b99d
Soumik <soumik20020906@gmail.com>
2023-08-22 18:26:06 +0530

    Marked up and down cars with different color

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 # importing librarys
 import cv2 as cv 
 import numpy as np
-from model1 import * 
+import model1  
 
 # loading video
 capture=cv.VideoCapture("example_video/test1.mp4")
 if not capture.isOpened() :
 	print("file path of Video is wrong.......")
 ret,frame=capture.read()
-frame=cv.resize(frame,(640,480))
+
+# loading first frame
+frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
 cars_count=0
 playing=True
+
+#Press D to Quit the video
+
+#video loop
 while playing:
 		# displaying each frame
 		if cv.waitKey(40) & 0xff ==ord('d'):
@@ -20,11 +26,13 @@ while playing:
 		# cv.imshow('Sample',frame)
 		ret,frame=capture.read()
 		if ret:
-			frame=cv.resize(frame,(640,480))
-			valid=find_cars(prev_frame,frame)
+			frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
+
+			# returns no of vehicles
+			valid=model1.find_cars(prev_frame,frame)
 			cv.putText(prev_frame,f"Vehicle Detected:{len(valid)}",(55,15),cv.FONT_HERSHEY_COMPLEX,0.6,(0,180,0),2)
 			cv.imshow('Sample2',prev_frame)
 		else :
 			playing=False
+
 capture.release()
-# cv.waitKey(0)
--- src/model1.py
+++ src/model1.py
@@ -1,7 +1,10 @@
-
 import cv2 as cv 
 import numpy as np
+
 algorithm=cv.bgsegm.createBackgroundSubtractorMOG()
+y_level_up=180
+y_level_down=310
+
 def find_cars(prev_frame,frame) :
 	prev_frame_gray=cv.cvtColor(prev_frame,cv.COLOR_BGR2GRAY);
 	frame_gray=cv.GaussianBlur(cv.cvtColor(frame,cv.COLOR_BGR2GRAY),(3,3),5);
@@ -10,13 +13,22 @@ def find_cars(prev_frame,frame) :
 	kernel=cv.getStructuringElement(cv.MORPH_ELLIPSE,(5,5))
 	dialated=cv.morphologyEx(dialated,cv.MORPH_CLOSE,kernel)
 	dialated=cv.morphologyEx(dialated,cv.MORPH_CLOSE,kernel)
-	valid=[]
 	contours,hierarchy=cv.findContours(dialated.copy(),cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+
+	valid=[]
 	for i,cntr in enumerate(contours) :
 		x,y,w,h=cv.boundingRect(cntr)
-		if  y>=310 and w>60 and h>60:
+		#right lane cars
+		if  y>=y_level_up and y<=y_level_down-h and x<=int(frame.shape[1])/2 and w>25 and h>25:
 			valid.append(cntr)
-			# cv.rectangle(prev_frame,(x,y),(x+w,y+h),(0,255,0),thickness=1)
-	cv.line(prev_frame,(0,254),(638,254),(0,255,0))
-	# cv.imshow('Dailated',dialated)
+			cv.rectangle(prev_frame,(x,y),(x+w,y+h),(0,255,0),thickness=1)
+
+		#left lane cars
+		if  y>=y_level_up and y<=y_level_down-h and x>int(frame.shape[1])/2 and w>25 and h>25:
+			valid.append(cntr)
+			cv.rectangle(prev_frame,(x,y),(x+w,y+h),(0,0,255),thickness=1)
+	#upper line
+	cv.line(prev_frame,(0,y_level_up),(int(frame.shape[1]),y_level_up),(0,255,0))
+	#lower line
+	cv.line(prev_frame,(0,y_level_down),(int(frame.shape[1]),y_level_down),(0,255,0))
 	return valid
