defb9c9cfdc403698528a727a9d471e427b4ad54
Soumik <soumik20020906@gmail.com>
2023-12-27 12:36:39 +0530

    bug fixes

--- src/main.py
+++ src/main.py
@@ -3,13 +3,17 @@ import cv2 as cv
 import model1  
 
 # loading video
+
 capture=cv.VideoCapture("example_video/test1.mp4")
-if not capture.isOpened() :
-	print("file path of Video is wrong.......")
+	
 ret,frame=capture.read()
 
 # loading first frame
-frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
+try:
+	frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
+except Exception as e:
+	print("file path of Video is wrong......." )
+	exit()
 cars_count=0
 playing=True
 
@@ -22,18 +26,16 @@ while playing:
 			playing=False
 			break
 		prev_frame=frame
-		# cv.imshow('Sample',frame)
 		ret,frame=capture.read()
-		if ret:
-			frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
+		frame=cv.resize(frame,(int(frame.shape[1]/2),int(frame.shape[0]/2)))
 
 			# returns no of vehicles
-			valid=model1.find_cars(prev_frame,frame)
-			cv.putText(prev_frame,f"Vehicle Detected:{len(valid)}",(55,15),cv.FONT_HERSHEY_COMPLEX,0.6,(0,180,0),2)
+		valid=model1.find_cars(prev_frame,frame)
+		cv.putText(prev_frame,f"Vehicle Detected:{len(valid)}",(55,15),cv.FONT_HERSHEY_COMPLEX,0.6,(0,180,0),2)
+		cv.imshow('Sample2',prev_frame)
+		# else :
+		# 	playing=False
 
-			cv.imshow('Sample2',prev_frame)
-		else :
-			playing=False
 
 capture.release()
 
